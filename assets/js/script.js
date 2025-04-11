dfdb9828aa8be96f062326c52c2fb1a11124783c
tajuddin1 <itstaj7272@gmail.com>
2025-04-11 16:59:44 +0600

    Fix some spacing issues and scss

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ const links = document.querySelectorAll('.btn-group .btn_link');
 
 links.forEach(link => {
   link.addEventListener('click', (e) => {
-    e.preventDefault();
 
     links.forEach(l => l.classList.remove('active'));
     link.classList.add('active');
@@ -12,7 +11,7 @@ links.forEach(link => {
 
     if (targetEl) {
       const isMobile = window.innerWidth <= 991;
-      const offset = isMobile ? 70 : 100; 
+      const offset = isMobile ? 70 : 175; 
 
       const topPos = targetEl.getBoundingClientRect().top + window.scrollY - offset;
 
@@ -23,3 +22,24 @@ links.forEach(link => {
     }
   });
 });
+
+
+
+  const toggleBtn = document.getElementById('faqAll');
+  let allOpen = false;
+
+  toggleBtn.addEventListener('click', function () {
+    const accordions = document.querySelectorAll('.accordion-collapse');
+
+    accordions.forEach((accordion) => {
+      const bsCollapse = bootstrap.Collapse.getInstance(accordion) || new bootstrap.Collapse(accordion, { toggle: false });
+
+      if (allOpen) {
+        bsCollapse.hide();
+      } else {
+        bsCollapse.show();
+      }
+    });
+    allOpen = !allOpen;
+
+  });
